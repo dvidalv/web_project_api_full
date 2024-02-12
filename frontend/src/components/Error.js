59e6b32ddc66a0f15da85cd2e42b607c6fd64ed2
dvidalv <dvidalv@gmail.com>
2024-02-12 14:46:17 -0600

    solving gitignore problem

--- frontend/src/components/Error.js
+++
@@ -1,9 +0,0 @@
-function Error({ error}) {
-	return (
-		<div style={{ color: 'red' }}>
-			<pre>{error}</pre>
-		</div>
-	)
-}
-
-export default Error
--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-	return (
-		<footer className="footer">
-			<p className="footer__title">&copy; 2023. David V. Vidal V.</p>
-		</footer>
-	);
-}
-
-export default Footer;
