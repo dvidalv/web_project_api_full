6fd5d5afb09aa31e8d4724c3725150c55234e5eb
dvidalv <dvidalv@gmail.com>
2024-02-13 16:15:36 -0600

    Update ImagePopup component

--- frontend/src/components/ImagePopup.js
+++ frontend/src/components/ImagePopup.js
@@ -1,28 +1,30 @@
 import CloseIcon from '../images/Close-Icon.svg';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import usePopup from '../hooks/usePopups';
 function ImagePopup({ card, onClose }) {
   const { handleOverlayClick } = usePopup(onClose);
 
   return (
-    <motion.div
-      className={`popup ${card.link && 'popup_opened'}`}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.7 }}
-      exit={{ opacity: 0 }}
-      onClick={handleOverlayClick}
-    >
-      <div className="popup__image-container">
-        <img src={card?.link} alt={card?.name} className="popup__image" />
-        <img
-          src={CloseIcon}
-          alt="close"
-          className="popup__close"
-          onClick={onClose}
-        />
-      </div>
-    </motion.div>
+    <AnimatePresence>
+      <motion.div
+        className={`popup ${card.link && 'popup_opened'}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.7 }}
+        exit={{ opacity: 0 }}
+        onClick={handleOverlayClick}
+      >
+        <div className="popup__image-container">
+          <img src={card?.link} alt={card?.name} className="popup__image" />
+          <img
+            src={CloseIcon}
+            alt="close"
+            className="popup__close"
+            onClick={onClose}
+          />
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
