ca65cea2e3e1fbb2643231123c4c6a5fd9c87568
dvidalv <dvidalv@gmail.com>
2024-02-13 21:58:27 -0600

    Update BASE_URL based on window location

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,9 @@
-const BASE_URL = 'http://localhost:3000';
-// const BASE_URL = 'https://api.alrededorusa.mooo.com';
+let BASE_URL;
+
+if (window.location.hostname === "localhost") {
+    BASE_URL = 'http://localhost:3000';
+} else {
+    BASE_URL = 'https://api.alrededorusa.mooo.com';
+}
+
 export default BASE_URL;
